e9e7a353aaa922d67538ba0f470bce5e5df4daf6
Tony Great <lovestony@outlook.com>
2019-03-30 15:38:09 +0800

    添加了两个RSA加密的方法 其中一个是从网上搜索来的,存在外来依赖包 新添加不含外来以来包的RSA加密方法

--- sample-rsa/src/main/java/cn/com/goolife/sample/rsa/sampletest/RsaTestSample.java
+++ sample-rsa/src/main/java/cn/com/goolife/sample/rsa/sampletest/RsaTestSample.java
@@ -1,8 +1,31 @@
 package cn.com.goolife.sample.rsa.sampletest;
 
+import cn.com.goolife.sample.rsa.RSA_goolife;
+
 public class RsaTestSample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		System.out.println("This is a sample of rsa.");
+		//初始化
+		RSA_goolife.init();
+		//获取私钥
+		String privateKey = RSA_goolife.getPrivateKey();
+		
+		System.out.println("privateKey:\n\t"+privateKey);
+		//获取公钥
+		String publicKey = RSA_goolife.getPublicKey();
+		
+		System.out.println("publicKey:\n\t"+publicKey);
+		//声明需要加密的字符
+		String plaintext = "去死吧";
+		
+		//加密
+		String ciphertext = RSA_goolife.encrypt(plaintext, publicKey);
+		
+		System.out.println("ciphertext:\n\t"+ciphertext);
+		//解密
+		String dPlaintext = RSA_goolife.decrypt(ciphertext, privateKey);
+		
+		System.out.println("dPlaintext:\n\t"+dPlaintext);
 	}
 }
